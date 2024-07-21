0f6fda8071e923d7177b197e0525a91bc6b56986
Pratik Vyas <>
2024-07-21 17:39:12 +0530

    no message

--- task07/src/lambdas/uuid_generator/handler.py
+++ task07/src/lambdas/uuid_generator/handler.py
@@ -50,8 +50,12 @@ class UuidGenerator(AbstractLambda):
             # file_name = f"{datetime.utcnow()}.json"
             dt = datetime.utcnow()
             # Format the datetime object to the desired ISO 8601 formats
-            iso_format_with_z = dt.strftime("%Y-%m-%d") + "T00:00:00.000Z"  
-            iso_format_with_microseconds = dt.strftime("%Y-%m-%d") + "T00:00:00.000000"
+            # iso_format_with_z = dt.strftime("%Y-%m-%dT%H:%M:%S.%f")[:-3] + "Z"  # For '2024-01-01T00:00:00.000Z'
+            # iso_format_with_microseconds = dt.strftime("%Y-%m-%dT%H:%M:%S.%f")  # For '2024-01-01T00:00:00.000000'
+            iso_format_with_z = dt.strftime("%Y-%m-%dT%H:%M") + ":00.000Z"  
+            iso_format_with_microseconds = dt.strftime("%Y-%m-%dT%H:%M") + ":00.000000"  
+
+
             # Combine the two formats with a '|'
             file_name = f"{iso_format_with_z}|{iso_format_with_microseconds}"
             print(">>file_name : "+file_name)
