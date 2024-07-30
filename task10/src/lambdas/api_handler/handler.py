c11434b8f25cc4ab4779e9b4280ef4c22e0c5714
Pratik Vyas <>
2024-07-30 21:32:38 +0530

    no message

---
+++ task10/src/lambdas/api_handler/handler.py
@@ -0,0 +1,271 @@
+from commons.log_helper import get_logger
+from commons.abstract_lambda import AbstractLambda
+
+_LOG = get_logger('ApiHandler-handler')
+
+
+import json
+import boto3
+import uuid
+import re
+from botocore.exceptions import ClientError
+
+cognito_client = boto3.client('cognito-idp')
+dynamodb = boto3.resource('dynamodb')
+tables_table = dynamodb.Table('cmtr-1bb19304-Tables')
+reservations_table = dynamodb.Table('cmtr-1bb19304-Reservations')
+my_pool_name="cmtr-1bb19304-simple-booking-userpool"
+
+USER_POOL_NAME="cmtr-1bb19304-simple-booking-userpool"
+
+
+
+class ApiHandler(AbstractLambda):
+
+    def validate_request(self, event) -> dict:
+        pass
+        
+    def handle_request(self, event, context):
+        """
+        Explain incoming event here
+        """
+        # todo implement business logic
+
+        # Initialize AWS services
+        try:
+            
+
+            print("----start-----")
+            print("Received event:", json.dumps(event))
+
+            
+            route=event['path']
+            print(route)
+
+            cognito_client = boto3.client('cognito-idp')
+            response = cognito_client.list_user_pools(MaxResults=60) 
+            print(response)
+
+            # booking-userpool: simple-booking-userpool
+
+            
+            
+
+            for pool in response['UserPools']:
+                # print(f"User Pool ID: {pool['Id']}, Name: {pool['Name']}")
+                if pool['Name']==USER_POOL_NAME:
+                    my_pool=pool
+               
+
+            print("--mypool--")
+            print(my_pool)
+            print(my_pool['Name']) 
+            print(my_pool['Id'])
+            USER_POOL_ID = str(my_pool['Id'])
+            print(USER_POOL_ID)
+            
+
+            
+
+            # cognito_client = boto3.client('cognito-idp')
+
+            # response = cognito_client.describe_user_pool(UserPoolId=USER_POOL_ID )
+            # # client_id = response['UserPool']['AppClientsToAttributeMapping'].keys()[0]
+            # print("---client")
+            # print(response)
+
+            # # app_clients = response['UserPool'].get('AppClients', [])
+            # app_clients = response['UserPool']["client-app"]
+            # print(app_clients)
+
+            CLIENT_ID="751v63k1sklgv56cc1vlcjjhr7"
+
+
+
+
+            if route == '/signup':
+                print("---1")
+                return signup(event,USER_POOL_ID)
+            elif route == '/signin':
+                print("---2")
+                return signin(event,USER_POOL_ID,CLIENT_ID)
+            elif route == '/table':
+                print("---3")
+                if event['httpMethod'] == 'POST':
+                    print("---3")
+                    return create_table(event)
+            elif route.startswith('/tables'):
+                print("---6")
+                if event['httpMethod'] == 'GET':
+                    print("---6")
+                    return get_tables(event)
+            elif route == '/reservation':
+                if event['httpMethod'] == 'POST':
+                    print("---7")
+                    return create_reservation(event)
+            elif route == '/reservations':
+                if event['httpMethod'] == 'GET':
+                    print("---8")
+                    return get_reservations(event)
+            
+           
+            print("---10---")
+            return {'statusCode': 404, 'body': json.dumps('Not Found')}
+
+
+     
+
+            return 200
+        except Exception as e:
+            print("----error")    
+            print(e)
+
+
+        return 200
+
+
+
+       
+    
+
+HANDLER = ApiHandler()
+
+
+def lambda_handler(event, context):
+    return HANDLER.lambda_handler(event=event, context=context)
+
+
+def signup(event,USER_POOL_ID):
+
+    print("---1-start")
+    body = json.loads(event['body'])
+    email = body['email']
+    password = body['password']
+    
+    # Validate email and password
+    if not re.match(r"[^@]+@[^@]+\.[^@]+", email):
+        return {'statusCode': 400, 'body': json.dumps('Invalid email')}
+    if len(password) < 12 or not re.search(r"[A-Za-z0-9$%^*]", password):
+        return {'statusCode': 400, 'body': json.dumps('Invalid password')}
+    
+    try:
+        print(USER_POOL_ID)
+        print(email)
+        print(password)
+        response = cognito_client.admin_create_user(
+            UserPoolId=USER_POOL_ID,
+            Username=email,
+            UserAttributes=[
+                {'Name': 'email', 'Value': email},
+            ],
+            MessageAction='SUPPRESS'  # Prevent sending a verification email
+        )
+        cognito_client.admin_set_user_password(
+            UserPoolId=USER_POOL_ID,
+            Username=email,
+            Password=password,
+            Permanent=True
+        )
+        print("---1-end")
+        return {'statusCode': 200, 'body': json.dumps('Sign-up successful')}
+    except ClientError as e:
+        print("--error--")
+        print(e)
+        # return {'statusCode': 400, 'body': json.dumps(str(e))}
+
+def signin(event,USER_POOL_ID,CLIENT_ID):
+
+    print("---2-signin-start")
+    body = json.loads(event['body'])
+    email = body['email']
+    password = body['password']
+    
+    try:
+        response = cognito_client.admin_initiate_auth(
+            UserPoolId=USER_POOL_ID,
+            ClientId=CLIENT_ID,
+            AuthFlow='ADMIN_NO_SRP_AUTH',
+            AuthParameters={
+                'USERNAME': email,
+                'PASSWORD': password,
+            }
+        )
+    
+        access_token = response['AuthenticationResult']['IdToken']
+                       
+
+        print("---2-signin--end")
+        return {'statusCode': 200, 'body': json.dumps({'accessToken': access_token})}
+    except ClientError as e:
+        return {'statusCode': 400, 'body': json.dumps(str(e))}
+
+from decimal import Decimal
+def convert_decimal_to_float(obj):
+    if isinstance(obj, Decimal):
+        return float(obj)
+    raise TypeError("Type not serializable")
+
+def get_tables(event):
+    print("---3-get_tables--start")
+    # Fetch tables from DynamoDB
+    response = tables_table.scan()
+   
+    print(response)
+    # item=convert_decimal_to_float(response['Items'])
+    print("---3-get_tables--end")
+    # return response['Items']
+    return {'statusCode': 200, 'body': json.dumps({'tables': response['Items'] }, default=convert_decimal_to_float)}
+    # return {'statusCode': 200, 'body': json.dumps({'tables': item})}
+
+def create_table(event):
+    body = json.loads(event['body'])
+    table_id = str(uuid.uuid4())
+    print("---4-create_table--start")
+    # Create a new table entry
+    item = {
+        'id': table_id,
+        'number': body['number'],
+        'places': body['places'],
+        'isVip': body['isVip'],
+        'minOrder': body.get('minOrder', None)
+    }
+    
+    tables_table.put_item(Item=item)
+    print("---4-create_table--end")
+    return {'statusCode': 200, 'body': json.dumps({'id': table_id})}
+
+
+def get_table(event, table_id):
+    response = tables_table.get_item(Key={'id': table_id})
+    if 'Item' in response:
+        return {'statusCode': 200, 'body': json.dumps(response['Item'])}
+    print("---5-get_table")
+    return {'statusCode': 400, 'body': json.dumps('Table not found')}
+
+def create_reservation(event):
+    body = json.loads(event['body'])
+    reservation_id = str(uuid.uuid4())
+    
+    print("---start 6-create_reservation-")
+    # Create a new reservation entry
+    item = {
+        'id': reservation_id,
+        'reservationId': reservation_id,
+        'tableNumber': body['tableNumber'],
+        'clientName': body['clientName'],
+        'phoneNumber': body['phoneNumber'],
+        'date': body['date'],
+        'slotTimeStart': body['slotTimeStart'],
+        'slotTimeEnd': body['slotTimeEnd']
+    }
+    print(item)
+    
+    reservations_table.put_item(Item=item)
+    print("---6-create_reservation-end")
+    return {'statusCode': 200, 'body': json.dumps({'reservationId': reservation_id})}
+
+def get_reservations(event):
+    # Fetch reservations from DynamoDB
+    response = reservations_table.scan()
+    print("---7-get_reservations")
+    return {'statusCode': 200, 'body': json.dumps({'reservations': response['Items']},default=convert_decimal_to_float)}
